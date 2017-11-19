5138a57e5270f6edf8457a3bc7787eff3208ca5c
Gauthier Brion <gauthier.brion@gmail.com>
2017-11-19 18:19:14 +0100

    WIP

--- main.go
+++ main.go
@@ -2,23 +2,68 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+type SensorRecord struct {
+	Timestamp   time.Time
+	Temperature float64
+	Humidity    float64
+}
+
+var sensors sync.Map
+
+var addr = flag.String("listen-address", ":1234", "The address to listen on for HTTP requests.")
 var (
 	nodeCounter = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "service_status",
 			Help: "Soa manager service status.",
 		},
-		[]string{"venture", "service", "satus", "resource"},
+		[]string{"venture", "service", "status", "resource"},
 	)
 )
+
+func sensorsHandler(w http.ResponseWriter, r *http.Request) {
+	room := r.URL.Query().Get("room")
+	if r.Method == "POST" {
+		//name := r.URL.Query().Get("name")
+		t := r.URL.Query().Get("type")
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println("Failed to read body!")
+			return
+		}
+
+		if t == "temperature;humidity" {
+			var sr SensorRecord
+			sr.Timestamp = time.Now()
+			if _, err := fmt.Sscanf(string(data), "%f;%f", &sr.Temperature, &sr.Humidity); err != nil {
+				log.Println("Failed to parse body!")
+				return
+			}
+
+			sensors.Store(room, sr)
+		}
+	} else if r.Method == "GET" {
+		var sr SensorRecord
+		if v, ok := sensors.Load(room); !ok {
+			log.Println("No temperature value found for ", room)
+		} else {
+			sr = v.(SensorRecord)
+		}
+
+		fmt.Fprintf(w, "%.2f\n%.2f\n%s", sr.Temperature, sr.Humidity, sr.Timestamp.String())
+	}
+}
 
 func main() {
 	prometheus.Unregister(prometheus.NewProcessCollector(os.Getpid(), ""))
@@ -26,5 +71,6 @@ func main() {
 	prometheus.MustRegister(nodeCounter)
 
 	http.Handle("/metrics", prometheus.Handler())
+	http.HandleFunc("/sensors", sensorsHandler)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
