b24fe893a5e60a3e831232f079c574c487046ed4
Gauthier Brion <gauthier.brion@gmail.com>
2018-09-28 00:25:46 +0200

    Write directly to InfluxDB

--- main.go
+++ main.go
@@ -8,9 +8,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/common/log"
-	"github.com/prometheus/common/version"
+	"github.com/influxdata/influxdb/client/v2"
+	log "github.com/sirupsen/logrus"
 )
 
 // SensorRecordExporter is the metrics exporter object.
@@ -20,9 +19,9 @@ type SensorRecordExporter struct{}
 // SensorRecord structure defining the values of a sensor
 type SensorRecord struct {
 	Timestamp   time.Time
-	Temperature float64
-	Humidity    float64
-	Power       float64
+	Temperature *float64
+	Humidity    *float64
+	Power       *float64
 }
 
 // PowerUsageRecord structure holds the amount of watts consummed in a period
@@ -51,76 +50,42 @@ var housePowerUsage struct {
 var housePowerUsage_mutex = &sync.Mutex{}
 
 var addr = flag.String("listen-address", ":2001", "The address to listen on for HTTP requests.")
-var (
-	scrapeDurationDesc = prometheus.NewDesc(
-		prometheus.BuildFQName(sensor_namespace, "scrape", "collector_duration_seconds"),
-		"sensor_exporter: Duration of a collector scrape.",
-		nil,
-		nil,
-	)
-	scrapeSuccessDesc = prometheus.NewDesc(
-		prometheus.BuildFQName(sensor_namespace, "scrape", "collector_success"),
-		"sensor_exporter: Whether the collector succeeded.",
-		nil,
-		nil,
-	)
-)
 
-// Describe implements the prometheus.Collector interface
-func (e *SensorRecordExporter) Describe(ch chan<- *prometheus.Desc) {
-	ch <- scrapeDurationDesc
-	ch <- scrapeSuccessDesc
-}
-
-// Collect implements the prometheus.Collector interface
-func (e *SensorRecordExporter) Collect(ch chan<- prometheus.Metric) {
-	now := time.Now().UTC()
+func collect() {
+	// Create a new point batch
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  "home_hub",
+		Precision: "s",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Returns sensor records
 	sensors_mutex.Lock()
 	defer sensors_mutex.Unlock()
 
 	for k, s := range sensors {
-		ch <- prometheus.NewMetricWithTimestamp(s.Timestamp,
-			prometheus.MustNewConstMetric(
-				prometheus.NewDesc(
-					sensor_namespace+"_temperature",
-					"The temperature of the room",
-					[]string{"room"},
-					nil),
-				prometheus.GaugeValue,
-				s.Temperature,
-				k,
-			),
-		)
-		if s.Humidity != 0 {
-			ch <- prometheus.NewMetricWithTimestamp(s.Timestamp,
-				prometheus.MustNewConstMetric(
-					prometheus.NewDesc(
-						sensor_namespace+"_humidity",
-						"The humidity of the room",
-						[]string{"room"},
-						nil),
-					prometheus.GaugeValue,
-					s.Humidity,
-					k,
-				),
-			)
+		fields := map[string]interface{}{}
+		if s.Temperature != nil {
+			fields["temperature"] = *s.Temperature
 		}
-		if s.Power != 0 {
-			ch <- prometheus.NewMetricWithTimestamp(s.Timestamp,
-				prometheus.MustNewConstMetric(
-					prometheus.NewDesc(
-						sensor_namespace+"_power",
-						"The power of the room",
-						[]string{"room"},
-						nil),
-					prometheus.GaugeValue,
-					s.Power,
-					k,
-				),
-			)
+		if s.Humidity != nil {
+			fields["humidity"] = *s.Humidity
 		}
+		if s.Power != nil {
+			fields["power"] = *s.Power
+		}
+
+		pt, err := client.NewPoint(
+			"sensors",
+			map[string]string{"room": k},
+			fields,
+			s.Timestamp)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bp.AddPoint(pt)
 
 		delete(sensors, k)
 	}
@@ -129,38 +94,21 @@ func (e *SensorRecordExporter) Collect(ch chan<- prometheus.Metric) {
 	housePowerUsage_mutex.Lock()
 	defer housePowerUsage_mutex.Unlock()
 
-	meanTime := housePowerUsage.From.Add(housePowerUsage.To.Sub(housePowerUsage.From) / 2)
-	description := prometheus.NewDesc(
-		house_power_usage,
-		"The usage of electrical lines",
-		[]string{"line"},
-		nil)
-
 	if !housePowerUsage.To.IsZero() {
-		ch <- prometheus.NewMetricWithTimestamp(meanTime,
-			prometheus.MustNewConstMetric(
-				description,
-				prometheus.GaugeValue,
-				housePowerUsage.L1,
-				"L1",
-			),
+		pt, err := client.NewPoint(
+			"house_power",
+			map[string]string{},
+			map[string]interface{}{
+				"L1": housePowerUsage.L1,
+				"L2": housePowerUsage.L2,
+				"L3": housePowerUsage.L3,
+			},
+			housePowerUsage.From.Add(housePowerUsage.To.Sub(housePowerUsage.From)/2),
 		)
-		ch <- prometheus.NewMetricWithTimestamp(meanTime,
-			prometheus.MustNewConstMetric(
-				description,
-				prometheus.GaugeValue,
-				housePowerUsage.L2,
-				"L2",
-			),
-		)
-		ch <- prometheus.NewMetricWithTimestamp(meanTime,
-			prometheus.MustNewConstMetric(
-				description,
-				prometheus.GaugeValue,
-				housePowerUsage.L3,
-				"L3",
-			),
-		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bp.AddPoint(pt)
 
 		housePowerUsage.From = housePowerUsage.To
 		housePowerUsage.To = time.Time{}
@@ -169,22 +117,12 @@ func (e *SensorRecordExporter) Collect(ch chan<- prometheus.Metric) {
 		housePowerUsage.L3 = 0
 	}
 
-	duration := time.Since(now)
-
-	var err error
-	err = nil
-
-	var success float64
-	if err != nil {
-		log.Errorf("ERROR: collector failed after %fs: %s", duration.Seconds(), err)
-		success = 0
-	} else {
-		log.Debugf("OK: collector succeeded after %fs", duration.Seconds())
-		success = 1
+	if len(bp.Points()) > 0 {
+		// Write the batch
+		if err := c.Write(bp); err != nil {
+			log.Fatal(err)
+		}
 	}
-
-	ch <- prometheus.MustNewConstMetric(scrapeDurationDesc, prometheus.GaugeValue, duration.Seconds())
-	ch <- prometheus.MustNewConstMetric(scrapeSuccessDesc, prometheus.GaugeValue, success)
 }
 
 func sensorsHandler(w http.ResponseWriter, r *http.Request) {
@@ -197,10 +135,11 @@ func sensorsHandler(w http.ResponseWriter, r *http.Request) {
 			log.Errorln("Failed to read body!")
 			return
 		}
+		str := string(data)
 
 		if t == "L1;L2;L3" {
 			var l1, l2, l3 float64
-			if _, err = fmt.Sscanf(string(data), "%f;%f;%f", &l1, &l2, &l3); err != nil {
+			if _, err = fmt.Sscanf(str, "%f;%f;%f", &l1, &l2, &l3); err != nil {
 				log.Errorln("Failed to parse body!")
 				return
 			}
@@ -228,13 +167,21 @@ func sensorsHandler(w http.ResponseWriter, r *http.Request) {
 			var sr SensorRecord
 			sr.Timestamp = now
 			if t == "temperature" {
-				_, err = fmt.Sscanf(string(data), "%f", &sr.Temperature)
+				sr.Temperature = new(float64)
+				_, err = fmt.Sscanf(str, "%f", sr.Temperature)
 			} else if t == "temperature;humidity" {
-				_, err = fmt.Sscanf(string(data), "%f;%f", &sr.Temperature, &sr.Humidity)
+				sr.Temperature = new(float64)
+				sr.Humidity = new(float64)
+				_, err = fmt.Sscanf(str, "%f;%f", sr.Temperature, sr.Humidity)
 			} else if t == "temperature;power" {
-				_, err = fmt.Sscanf(string(data), "%f;%f", &sr.Temperature, &sr.Power)
+				sr.Temperature = new(float64)
+				sr.Power = new(float64)
+				_, err = fmt.Sscanf(str, "%f;%f", sr.Temperature, sr.Power)
 			} else if t == "temperature;humidity;power" {
-				_, err = fmt.Sscanf(string(data), "%f;%f;%f", &sr.Temperature, &sr.Humidity, &sr.Power)
+				sr.Temperature = new(float64)
+				sr.Humidity = new(float64)
+				sr.Power = new(float64)
+				_, err = fmt.Sscanf(str, "%f;%f;%f", sr.Temperature, sr.Humidity, sr.Power)
 			} else {
 				log.Errorln("Unkown type!")
 				return
@@ -251,55 +198,41 @@ func sensorsHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		log.Debugln("Stored record for room: ", room)
-	} else if r.Method == "GET" {
-		var sr SensorRecord
-		var ok bool
-		sensors_mutex.Lock()
-		if sr, ok = sensors[room]; !ok {
-			log.Infoln("No temperature value found for room: ", room)
-			return
-		}
-		sensors_mutex.Unlock()
-
-		if sr.Power == 0 {
-			fmt.Fprintf(w, "%.2f\n%.2f\n%s", sr.Temperature, sr.Humidity, sr.Timestamp.String())
-		} else {
-			fmt.Fprintf(w, "%.2f\n%.2f\n%.2f\n%s", sr.Temperature, sr.Humidity, sr.Power, sr.Timestamp.String())
-		}
 	}
 }
 
-func init() {
-	// TODO - add this if you want to create a versioning of your program
-	// prometheus.MustRegister(version.NewCollector(program))
-	prometheus.Unregister(prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}))
-	prometheus.Unregister(prometheus.NewGoCollector())
+var c client.Client
 
-	// PUSH EXAMPLES
-	// sensors.Store("kitchen", SensorRecord{Timestamp: time.Now(), Temperature: 21.64, Humidity: 54.98})
-	// sensors.Store("hall", SensorRecord{Timestamp: time.Now(), Temperature: 20.01, Humidity: 54.12})
-	// sensors.Store("bedroom", SensorRecord{Timestamp: time.Now(), Temperature: 16.85, Humidity: 45.82})
-	// sensors.Store("laundry", SensorRecord{Timestamp: time.Now(), Temperature: 17.15, Humidity: 45.82})
+func init() {
+	// Create a new HTTPClient
+	var err error
+	c, err = client.NewHTTPClient(client.HTTPConfig{
+		Addr: "http://localhost:8086",
+		// Username: username,
+		// Password: password,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	q := client.NewQuery("CREATE DATABASE home_hub", "", "")
+	if response, err := c.Query(q); err == nil && response.Error() == nil {
+		log.Println(response.Results)
+	} else {
+		log.Fatal(response.Error())
+	}
 }
 
 func main() {
-	log.Infoln("Starting sensor reader/exporter", version.Info())
-	log.Infoln("Build context", version.BuildContext())
-
-	exporter := &SensorRecordExporter{}
-	prometheus.MustRegister(exporter)
-
-	http.Handle("/metrics", prometheus.UninstrumentedHandler())
 	http.HandleFunc("/sensors", sensorsHandler)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`<html>
-                        <head><title>SensorRecord Exporter</title></head>
-                        <body>
-                        <h1>SensorRecord Exporter</h1>
-                        <p><a href='/metrics'>Metrics</a></p>
-                        <p><a href='/sensors'>Sensors</a></p>
-                        </body>
-                        </html>`))
-	})
+
+	ticker := time.NewTicker(1 * time.Minute)
+	go func() {
+		for range ticker.C {
+			collect()
+		}
+	}()
+
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
