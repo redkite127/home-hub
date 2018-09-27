3fe7963a0801fd88e7e77f83c8c1052c885d3830
Gauthier Brion <gauthier.brion@gmail.com>
2018-09-27 22:53:04 +0200

    Add power records

--- main.go
+++ main.go
@@ -25,23 +25,41 @@ type SensorRecord struct {
 	Power       float64
 }
 
+// PowerUsageRecord structure holds the amount of watts consummed in a period
+type PowerUsageRecord struct {
+	From  time.Time
+	To    time.Time
+	Watts float64
+}
+
 const (
-	namespace = "sensor"
-	program   = "sensor_record"
+	sensor_namespace  = "sensor"
+	house_power_usage = "house_power_usage_watts"
+	program           = "sensor_record"
 )
 
-var sensors sync.Map
+var sensors = map[string]SensorRecord{}
+var sensors_mutex = &sync.Mutex{}
+
+var housePowerUsage struct {
+	L1   float64
+	L2   float64
+	L3   float64
+	From time.Time
+	To   time.Time
+}
+var housePowerUsage_mutex = &sync.Mutex{}
 
 var addr = flag.String("listen-address", ":2001", "The address to listen on for HTTP requests.")
 var (
 	scrapeDurationDesc = prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, "scrape", "collector_duration_seconds"),
+		prometheus.BuildFQName(sensor_namespace, "scrape", "collector_duration_seconds"),
 		"sensor_exporter: Duration of a collector scrape.",
 		nil,
 		nil,
 	)
 	scrapeSuccessDesc = prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, "scrape", "collector_success"),
+		prometheus.BuildFQName(sensor_namespace, "scrape", "collector_success"),
 		"sensor_exporter: Whether the collector succeeded.",
 		nil,
 		nil,
@@ -56,63 +74,102 @@ func (e *SensorRecordExporter) Describe(ch chan<- *prometheus.Desc) {
 
 // Collect implements the prometheus.Collector interface
 func (e *SensorRecordExporter) Collect(ch chan<- prometheus.Metric) {
-	begin := time.Now()
+	now := time.Now().UTC()
 
-	sensors.Range(func(k, v interface{}) bool {
-		// Don't send this metric if it is too old (probe not comunicating anymore)
-		if time.Now().Sub(v.(SensorRecord).Timestamp) > 15*time.Minute {
-			return true
-		}
+	// Returns sensor records
+	sensors_mutex.Lock()
+	defer sensors_mutex.Unlock()
 
-		recordTime := v.(SensorRecord).Timestamp
-		t := v.(SensorRecord).Temperature
-		h := v.(SensorRecord).Humidity
-		p := v.(SensorRecord).Power
-
-		ch <- prometheus.NewMetricWithTimestamp(recordTime,
+	for k, s := range sensors {
+		ch <- prometheus.NewMetricWithTimestamp(s.Timestamp,
 			prometheus.MustNewConstMetric(
 				prometheus.NewDesc(
-					prometheus.BuildFQName(namespace, "", "temperature"),
+					sensor_namespace+"_temperature",
 					"The temperature of the room",
 					[]string{"room"},
 					nil),
 				prometheus.GaugeValue,
-				t,
-				k.(string),
+				s.Temperature,
+				k,
 			),
 		)
-		if h != 0 {
-			ch <- prometheus.NewMetricWithTimestamp(recordTime,
+		if s.Humidity != 0 {
+			ch <- prometheus.NewMetricWithTimestamp(s.Timestamp,
 				prometheus.MustNewConstMetric(
 					prometheus.NewDesc(
-						prometheus.BuildFQName(namespace, "", "humidity"),
+						sensor_namespace+"_humidity",
 						"The humidity of the room",
 						[]string{"room"},
 						nil),
 					prometheus.GaugeValue,
-					h,
-					k.(string),
+					s.Humidity,
+					k,
 				),
 			)
 		}
-		if p != 0 {
-			ch <- prometheus.NewMetricWithTimestamp(recordTime,
+		if s.Power != 0 {
+			ch <- prometheus.NewMetricWithTimestamp(s.Timestamp,
 				prometheus.MustNewConstMetric(
 					prometheus.NewDesc(
-						prometheus.BuildFQName(namespace, "", "power"),
+						sensor_namespace+"_power",
 						"The power of the room",
 						[]string{"room"},
 						nil),
 					prometheus.GaugeValue,
-					p,
-					k.(string),
+					s.Power,
+					k,
 				),
 			)
 		}
-		return true
-	})
 
-	duration := time.Since(begin)
+		delete(sensors, k)
+	}
+
+	// Returns power records
+	housePowerUsage_mutex.Lock()
+	defer housePowerUsage_mutex.Unlock()
+
+	meanTime := housePowerUsage.From.Add(housePowerUsage.To.Sub(housePowerUsage.From) / 2)
+	description := prometheus.NewDesc(
+		house_power_usage,
+		"The usage of electrical lines",
+		[]string{"line"},
+		nil)
+
+	if !housePowerUsage.To.IsZero() {
+		ch <- prometheus.NewMetricWithTimestamp(meanTime,
+			prometheus.MustNewConstMetric(
+				description,
+				prometheus.GaugeValue,
+				housePowerUsage.L1,
+				"L1",
+			),
+		)
+		ch <- prometheus.NewMetricWithTimestamp(meanTime,
+			prometheus.MustNewConstMetric(
+				description,
+				prometheus.GaugeValue,
+				housePowerUsage.L2,
+				"L2",
+			),
+		)
+		ch <- prometheus.NewMetricWithTimestamp(meanTime,
+			prometheus.MustNewConstMetric(
+				description,
+				prometheus.GaugeValue,
+				housePowerUsage.L3,
+				"L3",
+			),
+		)
+
+		housePowerUsage.From = housePowerUsage.To
+		housePowerUsage.To = time.Time{}
+		housePowerUsage.L1 = 0
+		housePowerUsage.L2 = 0
+		housePowerUsage.L3 = 0
+	}
+
+	duration := time.Since(now)
 
 	var err error
 	err = nil
@@ -131,6 +188,7 @@ func (e *SensorRecordExporter) Collect(ch chan<- prometheus.Metric) {
 }
 
 func sensorsHandler(w http.ResponseWriter, r *http.Request) {
+	now := time.Now().UTC()
 	room := r.URL.Query().Get("room")
 	if r.Method == "POST" {
 		t := r.URL.Query().Get("type")
@@ -140,35 +198,68 @@ func sensorsHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		var sr SensorRecord
-		sr.Timestamp = time.Now()
-		if t == "temperature" {
-			_, err = fmt.Sscanf(string(data), "%f", &sr.Temperature)
-		} else if t == "temperature;humidity" {
-			_, err = fmt.Sscanf(string(data), "%f;%f", &sr.Temperature, &sr.Humidity)
-		} else if t == "temperature;power" {
-			_, err = fmt.Sscanf(string(data), "%f;%f", &sr.Temperature, &sr.Power)
-		} else if t == "temperature;humidity;power" {
-			_, err = fmt.Sscanf(string(data), "%f;%f;%f", &sr.Temperature, &sr.Humidity, &sr.Power)
+		if t == "L1;L2;L3" {
+			var l1, l2, l3 float64
+			if _, err = fmt.Sscanf(string(data), "%f;%f;%f", &l1, &l2, &l3); err != nil {
+				log.Errorln("Failed to parse body!")
+				return
+			}
+
+			housePowerUsage_mutex.Lock()
+			defer housePowerUsage_mutex.Unlock()
+
+			if housePowerUsage.From.IsZero() {
+				// I received an amount of power consummed, but I don't know since when...
+				// ==> trash it, but init From
+				housePowerUsage.From = now
+				return
+			} else if now.Sub(housePowerUsage.From) > 2*time.Minute {
+				// Too much time between from & to, there was probably a problem
+				// ==> trash it, but init From
+				housePowerUsage.From = now
+				return
+			} else {
+				housePowerUsage.L1 += l1
+				housePowerUsage.L2 += l2
+				housePowerUsage.L3 += l3
+				housePowerUsage.To = now
+			}
 		} else {
-			log.Errorln("Unkown type!")
-			return
+			var sr SensorRecord
+			sr.Timestamp = now
+			if t == "temperature" {
+				_, err = fmt.Sscanf(string(data), "%f", &sr.Temperature)
+			} else if t == "temperature;humidity" {
+				_, err = fmt.Sscanf(string(data), "%f;%f", &sr.Temperature, &sr.Humidity)
+			} else if t == "temperature;power" {
+				_, err = fmt.Sscanf(string(data), "%f;%f", &sr.Temperature, &sr.Power)
+			} else if t == "temperature;humidity;power" {
+				_, err = fmt.Sscanf(string(data), "%f;%f;%f", &sr.Temperature, &sr.Humidity, &sr.Power)
+			} else {
+				log.Errorln("Unkown type!")
+				return
+			}
+
+			if err != nil {
+				log.Errorln("Failed to parse body!")
+				return
+			}
+
+			sensors_mutex.Lock()
+			sensors[room] = sr
+			sensors_mutex.Unlock()
 		}
 
-		if err != nil {
-			log.Errorln("Failed to parse body!")
-			return
-		}
-
-		sensors.Store(room, sr)
 		log.Debugln("Stored record for room: ", room)
 	} else if r.Method == "GET" {
 		var sr SensorRecord
-		if v, ok := sensors.Load(room); !ok {
+		var ok bool
+		sensors_mutex.Lock()
+		if sr, ok = sensors[room]; !ok {
 			log.Infoln("No temperature value found for room: ", room)
-		} else {
-			sr = v.(SensorRecord)
+			return
 		}
+		sensors_mutex.Unlock()
 
 		if sr.Power == 0 {
 			fmt.Fprintf(w, "%.2f\n%.2f\n%s", sr.Temperature, sr.Humidity, sr.Timestamp.String())
