cd4bd25df349962f171a4a2cc376fd5fc738d42d
Gauthier Brion <g.brion@tadaweb.com>
2020-12-13 20:44:39 +0100

    use config file for toc sentence & sonos API uri

--- sonos.go
+++ sonos.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"io/ioutil"
 	"net/http"
 	"net/url"
-	"strings"
+
+	"github.com/spf13/viper"
 )
 
 func PlayTocToc() {
-
-	str := "Hi"
-	dat, err := ioutil.ReadFile("./toc_toc.txt")
-	if err == nil {
-		str = strings.TrimSuffix(string(dat), "\n")
-	}
-	go http.Get("http://10.161.0.111:5005/sayall/" + url.PathEscape(str) + "/fr/65")
+	//str := "toc toc toc"
+	str := viper.GetString("toc_toc")
+	go http.Get(viper.GetString("sonos_api_uri") + "/sayall/" + url.PathEscape(str) + "/fr/65")
 }
